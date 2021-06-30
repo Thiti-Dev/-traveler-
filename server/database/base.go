2339c0a6b621801921afdcda932046336ba6774f
Thiti Dev <thiti.mwk.main@gmail.com>
2021-06-30 19:34:16 +0700

    created (base)postgres-db

---
+++ server/database/base.go
@@ -0,0 +1,66 @@
+package database
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/Thiti-Dev/traveller/database/models"
+	"github.com/go-pg/pg/v10"
+	"github.com/go-pg/pg/v10/orm"
+)
+
+//Globally Accessible
+var databaseConnection *pg.DB
+// ────────────────────────────────────────────────────────────────────────────────
+
+// ─── INSTANCE METHODS ───────────────────────────────────────────────────────────
+func createSchema(db *pg.DB) error {
+    models := []interface{}{
+        (*models.User)(nil),
+    }
+
+    for _, model := range models {
+        err := db.Model(model).CreateTable(&orm.CreateTableOptions{
+            Temp: false, // Not temporary
+			IfNotExists: true,
+        })
+        if err != nil {
+            return err
+        }
+    }
+    return nil
+}
+// ────────────────────────────────────────────────────────────────────────────────
+
+
+
+func CreatePostgresConnection() {
+	db := pg.Connect(&pg.Options{
+		Addr:     ":5432",
+		User:     "postgres",
+		Password: "root",
+		Database: "traveller",
+	})
+
+	// Creating table from declared model
+	err := createSchema(db)
+    if err != nil {
+        panic(err)
+    }
+	// ────────────────────────────────────────────────────────────────────────────────
+
+
+	// Check if db is up and running
+	ctx := context.Background()
+	if err := db.Ping(ctx); err != nil {
+		panic(err)
+	}
+	// ────────────────────────────────────────────────────────────────────────────────
+
+	fmt.Printf("Connected to postgresDB: %v\n",db)
+	databaseConnection = db
+}
+
+func GetEstablishedPostgresConnection() *pg.DB{
+	return databaseConnection
+}
