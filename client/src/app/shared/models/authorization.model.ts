7a863f2d408cb30a8bb98882858c5f7de6119122
Thiti Dev <thiti.mwk.main@gmail.com>
2021-07-06 03:20:32 +0700

    interface: created ITokenSignature

--- client/src/app/shared/models/authorization.model.ts
+++ client/src/app/shared/models/authorization.model.ts
@@ -2,3 +2,10 @@ export interface IAuthCredentials {
   username: string;
   password: string;
 }
+
+export interface ITokenSignature {
+  username: string;
+  _id: number;
+  exp: number;
+  iss: string;
+}
