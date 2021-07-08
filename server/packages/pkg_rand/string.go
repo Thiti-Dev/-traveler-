f9d55d458f57cd9f218997b1f06b6ea82b10b3f5
Thiti Dev <thiti.mwk.main@gmail.com>
2021-07-08 19:28:32 +0700

    implemented seed phase for pkg_rand

--- server/packages/pkg_rand/string.go
+++ server/packages/pkg_rand/string.go
@@ -1,12 +1,22 @@
 package pkg_rand
 
-import "math/rand"
+import (
+	"math/rand"
+	"time"
+)
 
-var seed_needed bool = false
+
+var seed_needed bool = true
 
 var letterRunes = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
 
 func RandStringRunes(n int) string {
+
+	if seed_needed{
+		rand.Seed(time.Now().UnixNano())
+		seed_needed = false
+	}
+
 	b := make([]rune, n)
 	for i := range b {
 		b[i] = letterRunes[rand.Intn(len(letterRunes))]
