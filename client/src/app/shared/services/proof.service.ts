95a7b2f9a3a7932b27bde99f909b16d3d3ad728e
Thiti Dev <thiti.mwk.main@gmail.com>
2021-07-16 18:14:53 +0700

    proof_service ->implemented getActiveThreads,proofTheSpecificThread

--- client/src/app/shared/services/proof.service.ts
+++ client/src/app/shared/services/proof.service.ts
@@ -1,16 +1,22 @@
 import { Injectable } from '@angular/core';
 
 import { ProofServiceClient } from '../../proto/proof_pb_service';
-import { ProofThread, GetAllProofThreadResponse } from '../../proto/proof_pb';
+import {
+  ProofThread,
+  GetAllProofThreadResponse,
+  ProofingRequest,
+  ProofingResponse,
+} from '../../proto/proof_pb';
 import { grpc } from '@improbable-eng/grpc-web';
 
 import * as google_protobuf_empty_pb from 'google-protobuf/google/protobuf/empty_pb.js';
 import { IResponseMapped } from '../interfaces/common.interface';
+import { AuthService } from './auth.service';
 
 @Injectable({ providedIn: 'root' })
 export class ProofService {
   client: ProofServiceClient;
-  constructor() {
+  constructor(private authService: AuthService) {
     this.client = new ProofServiceClient('http://localhost:8080'); // envoy proxy server
   }
 
@@ -26,7 +32,29 @@ export class ProofService {
           resolve({ success: false });
           return;
         }
-        console.log(response!.toObject().proofThreadsList);
+        //console.log(response!.toObject().proofThreadsList);
+        resolve({ success: true, data: response!.toObject() });
+      });
+    });
+  }
+
+  public proofTheSpecificThread(
+    thread_id: number,
+    code: string
+  ): Promise<IResponseMapped<ProofingResponse.AsObject>> {
+    return new Promise((resolve, reject) => {
+      const req = new ProofingRequest();
+      req.setThreadId(thread_id);
+      req.setSecretCode(code);
+      const metadata = new grpc.Metadata();
+      metadata.set('authorization', localStorage.getItem('token')!);
+      this.client.proofing(req, metadata, (err, response) => {
+        if (err) {
+          console.log(err);
+          resolve({ success: false });
+          return;
+        }
+        //console.log(response!.toObject());
         resolve({ success: true, data: response!.toObject() });
       });
     });
